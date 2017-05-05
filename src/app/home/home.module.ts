07e30cc8527ce90c5b8c67461b73ff92a7196d92
ridvanc <ridvanc07@gmail.com>
2017-05-05 14:05:05 +0300

    change files

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { MODULE_COMPONENTS, MODULE_ROUTES } from './home.routes';
-import { AboutComponent } from './about/about.component';
-import { ContactComponent } from './contact/contact.component';
+import { MODULE_COMPONENTS, MODULE_ROUTES } from './home.routes'; 
 @NgModule({
   imports: [
     CommonModule, 
